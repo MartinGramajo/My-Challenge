3a40a1b1a99a982485857b388cd54e46b1be523f
magus <martingramajo08@gmail.com>
2021-08-22 15:58:04 -0300

    agrego iconos font en Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBriefcase, faBullhorn, faMedal, faStar } from '@fortawesome/free-solid-svg-icons'
 export default function Banner() {
     return (
         <div className="bg-dark text-white p-5">
             <div className="container">
                 <div className="row">
                     <div className="col-3 text-center">
-                        <h1> 100+</h1>
+                        <h1> <FontAwesomeIcon icon={faBullhorn} /> 100+</h1>
                         <p> PROJECTS COMPLETED</p>
                     </div>
                     <div className="col-3 text-center ">
-                        <h1> 4.9 </h1>
+                        <h1> <FontAwesomeIcon icon={faStar} /> 4.9 </h1>
                         <p> AVERAGE RATING</p>
                     </div>
                     <div className="col-3 text-center">
-                        <h1> 15+ </h1>
+                        <h1> <FontAwesomeIcon icon={faBriefcase} /> 15+ </h1>
                         <p> HARD WORKING PEOPLE</p>
                     </div>
                     <div className="col-3 text-center">
-                        <h1> 250+ </h1>
+                        <h1> <FontAwesomeIcon icon={faMedal} /> 250+ </h1>
                         <p> SATISFIED CLIENTS </p>
                     </div>
                 </div>
