4cd453fd071d33558d21ef485711c73401eef39c
magus <martingramajo08@gmail.com>
2021-08-25 18:10:26 -0300

    agrego iconos de font a componente Comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -7,16 +7,22 @@ export default function Comment({ data }) {
     const { image, nombre, comentario } = data;
     return (
         <div className="my-5 mx-1">
-            <Toast className="col-6">
-                <Toast.Header>
-                    <Image src={ image } style={{ width: '80px' }} roundedCircle />
-                    <strong className="me-auto m-auto"> { nombre }</strong>
-                </Toast.Header>
-                <Toast.Body>
-                <p>Service: 5 <FontAwesomeIcon icon={faStar} /> </p>
-                { comentario }
-                </Toast.Body>
-            </Toast>
+            <div className="container">
+                <Toast className="col-6">
+                    <Toast.Header>
+                        <Image src={image} style={{ width: '80px' }} roundedCircle />
+                        <strong className="me-auto m-auto"> {nombre}</strong>
+                    </Toast.Header>
+                    <Toast.Body>
+                        <p>Service: <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} /></p>
+                        {comentario}
+                    </Toast.Body>
+                </Toast>
+            </div>
         </div>
     )
 }
