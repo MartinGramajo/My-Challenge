aae2bdcc8652c19cbb06f068e046f4fd730222de
magus <martingramajo08@gmail.com>
2021-08-25 17:32:56 -0300

    cambio contenido en componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,13 @@ import { Form, Button } from 'react-bootstrap'
 export default function Header() {
     return (
         <div className="d-flex flex-wrap fondo-imagen">
-            <div className="text-white col-lg-6 col-sm-12 text-center m-auto">
-                <h1>Tu compañia perfecta</h1>
-                <p> No solo para tus desayuno y meriendas, nuestros productos estan presentes en cada momento de tu vida.</p>
+            <div className="text-white col-lg-6 col-sm-12 text-center my-auto">
+                <h1>Let Us Solve Your</h1>
+                <h2> Roofing Problems</h2>
+                <p> The best customer in the business, no joke!</p>
+                <p> Check it out by yourself right now.</p>
                 <Button className="mt-3" variant="danger" type="submit">
-                    Click here to submit form
+                    Call Us Now!
                 </Button>
             </div>
             <div style={{
@@ -19,20 +21,32 @@ export default function Header() {
                 padding: 30
             }}>
                 <Form className="bg-light p-5">
-                    <Form.Group>
-                        <Form.Label>Enter your full name:</Form.Label>
+                    <div className="text-center">
+                        <h1> Get An Estimate</h1>
+                        <p> Need to Know much your cost?</p>
+                    </div>
+                    <Form.Group className="mb-3">
                         <Form.Control type="text"
-                            placeholder="Enter your full name" />
+                            placeholder="name" />
                     </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Enter your email address:</Form.Label>
+                    <Form.Group className="mb-3">
                         <Form.Control type="email"
-                            placeholder="Enter your your email address" />
+                            placeholder="E-mail*" />
                     </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Enter your age:</Form.Label>
-                        <Form.Control type="number" placeholder="Enter your age" />
+                    <Form.Group className="mb-3">
+                        <Form.Control type="number" placeholder="Phone*" />
                     </Form.Group>
+                    <Form.Select aria-label="Default select example">
+                        <option>Interested In</option>
+                        <option value="1">Roof Repair</option>
+                        <option value="2">Skylight Installation</option>
+                        <option value="3">Solar Roof Ventilator</option>
+                        <option value="4">Downspout Service</option>
+                        <option value="5">Flat Roof Installation</option>
+                        <option value="6">Shingles Roof</option>
+                        <option value="7">Gutter Cleaning Service</option>
+                        <option value="8">Chimmey Repair Service</option>
+                    </Form.Select>
                     <Button className="mt-3" variant="danger" type="submit">
                         Click here to submit form
                     </Button>
