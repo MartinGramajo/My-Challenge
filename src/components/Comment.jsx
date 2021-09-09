0f651b33111bda27e01bae6637773fc7a70998c3
magus <martingramajo08@gmail.com>
2021-09-09 14:31:52 -0300

    agrego efecto hover a comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './comment.css'
 import { Toast, Image } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
@@ -6,7 +7,7 @@ import { faStar } from '@fortawesome/free-solid-svg-icons'
 export default function Comment({ data }) {
     const { image, nombre, comentario } = data;
     return (
-        <div className="my-5 mx-1">
+        <div className="my-5 mx-1 hover">
             <div className="container">
                 <Toast className="col-6">
                     <Toast.Header>
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -18,11 +18,26 @@ export default function Comments() {
             nombre: "Emma S.",
             comentario: "“JR roofing did a great job on our roof. Brian was very responsive and professional in my initial request for a bid on replacing our old roof. His detailed proposal and followup emails provided several options and competitive prices.”"
         },
+        {
+            image: "https://images.unsplash.com/photo-1518991669955-9c7e78ec80ca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+            nombre: "Emma S.",
+            comentario: "“JR roofing did a great job on our roof. Brian was very responsive and professional in my initial request for a bid on replacing our old roof. His detailed proposal and followup emails provided several options and competitive prices.”"
+        },
+        {
+            image: "https://images.unsplash.com/photo-1518991669955-9c7e78ec80ca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+            nombre: "Emma S.",
+            comentario: "“JR roofing did a great job on our roof. Brian was very responsive and professional in my initial request for a bid on replacing our old roof. His detailed proposal and followup emails provided several options and competitive prices.”"
+        },
+        {
+            image: "https://images.unsplash.com/photo-1518991669955-9c7e78ec80ca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+            nombre: "Emma S.",
+            comentario: "“JR roofing did a great job on our roof. Brian was very responsive and professional in my initial request for a bid on replacing our old roof. His detailed proposal and followup emails provided several options and competitive prices.”"
+        },
     ]
     return (
         <>
             
-            <div className=" container d-flex  flex-wrap justify-content-around">
+            <div className=" container d-flex  flex-wrap justify-content-between">
                 {perfiles.map((perfil, i) =>
                     <Comment key={i} data={perfil} />
                 )}
